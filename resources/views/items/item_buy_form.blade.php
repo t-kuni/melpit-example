2aceeb19eb38c2fde98bacff39c8728731f510e7
t-kuni <t.kuni.dev@gmail.com>
2020-12-05 13:48:16 +0900

    決済フォーム実装

--- resources/views/items/item_buy_form.blade.php
+++ resources/views/items/item_buy_form.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
+<script src="https://js.pay.jp/v2/pay.js"></script>
 <div class="container">
     <div class="row">
         <div class="col-8 offset-2 bg-white">
@@ -40,9 +41,9 @@
                 </div>
             </div>
 
-            <div class="row mt-3">
+            <div class="row mt-3 mb-3">
                 <div class="col-8 offset-2">
-                    <button class="btn btn-secondary btn-block">購入</button>
+                    <button class="btn btn-secondary btn-block" onclick="onSubmit(event)">購入</button>
                 </div>
             </div>
 
@@ -53,4 +54,32 @@
         </div>
     </div>
 </div>
+<script>
+    var payjp = Payjp('{{config("payjp.public_key")}}')
+
+    var elements = payjp.elements()
+
+    var numberElement = elements.create('cardNumber')
+    var expiryElement = elements.create('cardExpiry')
+    var cvcElement = elements.create('cardCvc')
+    numberElement.mount('#number-form')
+    expiryElement.mount('#expiry-form')
+    cvcElement.mount('#cvc-form')
+
+    function onSubmit(event) {
+        const msgDom = document.querySelector('.card-form-alert');
+        msgDom.style.display = "none";
+
+        payjp.createToken(numberElement).then(function(r) {
+            if (r.error) {
+                msgDom.innerText = r.error.message;
+                msgDom.style.display = "block";
+                return;
+            }
+
+            document.querySelector('#card-token').value = r.id;
+            document.querySelector('#buy-form').submit();
+        })
+    }
+</script>
 @endsection
