7bc594685eb189aeed6549ed1b9838daacc9d439
t-kuni <t.kuni.dev@gmail.com>
2020-12-18 01:28:49 +0900

    トップに出品ボタン追加

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -36,4 +36,15 @@
             {{ $items->withQueryString()->links() }}
         </div>
     </div>
+
+    <a href="{{route('sell')}}"
+       class="bg-secondary text-white d-inline-block d-flex justify-content-center align-items-center flex-column"
+       role="button"
+       style="position: fixed; bottom: 30px; right: 30px; width: 150px; height: 150px; border-radius: 75px;"
+    >
+        <div style="font-size: 24px;">出品</div>
+        <div>
+            <i class="fas fa-camera" style="font-size: 30px;"></i>
+        </div>
+    </a>
 @endsection
